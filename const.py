7a7d9234d370d87c7c396c03057d99aaf24414e0
Cassio Emanuel <cassioemanuel888@gmail.com>
2025-07-07 15:39:37 -0300

    fixing

--- const.py
+++ const.py
@@ -4,7 +4,7 @@ from background import pista
 
 WIN_WIDTH, WIN_HEIGHT = pista.get_width(), pista.get_height()
 
-
+#Mudar tamanho da imagem
 def SCALE_IMAGE(img, factor):
     size = round(img.get_width() * factor), round(img.get_height() * factor)
     return pygame.transform.scale(img, size)
--- main.py
+++ main.py
@@ -1,31 +1,36 @@
 import pygame
 import random
-import time
 import carros
-import math
 from background import background
-from background import pista
-from carros import Inimigos
 from const import WIN_WIDTH, WIN_HEIGHT
 
 pygame.init()
+
+menu_background = pygame.image.load("assets/fundo_menuprincipal.png")
+menu_background = pygame.transform.scale(menu_background, (WIN_WIDTH, WIN_HEIGHT))
+# Música do menu
+pygame.mixer.music.load("assets/music.mp3")
+pygame.mixer.music.set_volume(0.3)
+pygame.mixer.music.play(-1)
+
+
 def mostrar_menu():
-    titulo_fonte = pygame.font.SysFont("Arial", 60, bold=True)
-    opcao_fonte = pygame.font.SysFont("Arial", 36)
+    titulo_fonte = pygame.font.Font("assets/Pixel_Emulator.otf", 60)
+    opcao_fonte = pygame.font.Font("assets/Pixel_Emulator.otf", 36)
 
     opcoes = ["Fácil", "Médio", "Difícil"]
     selecionado = 0
 
     while True:
-        window.fill((0, 0, 0))
+        window.blit(menu_background, (0, 0))
 
         # Título do jogo
-        titulo = titulo_fonte.render("Traffic Boom", True, (255, 255, 0))
+        titulo = titulo_fonte.render("Traffic Boom", True, (25, 25, 255))
         window.blit(titulo, (WIN_WIDTH // 2 - titulo.get_width() // 2, 100))
 
         # Opções de dificuldade
         for i, texto in enumerate(opcoes):
-            cor = (255, 0, 0) if i == selecionado else (255, 255, 255)
+            cor = (0, 0, 255) if i == selecionado else (255, 255, 255)
             opcao_render = opcao_fonte.render(texto, True, cor)
             window.blit(opcao_render, (WIN_WIDTH // 2 - opcao_render.get_width() // 2, 250 + i * 50))
 
@@ -46,6 +51,7 @@ def mostrar_menu():
 
 window = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
 
+som_batida = pygame.mixer.Sound("assets/8-bit-explosion-3-340456.wav")
 
 FPS = 120
 
@@ -57,6 +63,11 @@ clock = pygame.time.Clock()
 bg = background(window)
 # Mostra o menu
 dificuldade = mostrar_menu()
+pygame.mixer.music.stop()
+pygame.mixer.init()
+pygame.mixer.music.load("assets/8bitmusic.mp3")
+pygame.mixer.music.set_volume(0.2)
+pygame.mixer.music.play(-1)
 
 # Definições de dificuldade
 if dificuldade == "Fácil":
@@ -80,15 +91,13 @@ elif dificuldade == "Difícil":
 score = 0
 tempo_inicio = pygame.time.get_ticks()
 
-#inimigos carros
+# inimigos carros
 inimigos_ativos = []
 tempo_ultimo_spawn = pygame.time.get_ticks()
 
-
-#inimigo mina
+# inimigo mina
 minas_ativas = []
 tempo_ultimo_spawn_mina = pygame.time.get_ticks()
-
 
 while run:
     clock.tick(FPS)
@@ -114,27 +123,26 @@ while run:
             carros.carro1_rect.x += vel
         hitbox_carro1 = carros.carro1_rect.inflate(-80, -20)
 
-    #spawn de carros
+        # spawn de carros
         if agora - tempo_ultimo_spawn > tempo_spawn_intervalo:
             tempo_ultimo_spawn = agora
-            faixas = [238, 364, 482, 598]#principais
-            #faixas = [230, 330, 360, 363, 480, 485, 598, 620]
+            faixas = [238, 364, 482, 598]  # principais
+            # faixas = [230, 330, 360, 363, 480, 485, 598, 620]
             x = random.choice(faixas)
             img = random.choice(carros.inimigos_imgs)
             largura = img.get_width()
-          # dentro da pista
+            # dentro da pista
 
             novo_inimigo = carros.Inimigos(x, -100, img, velocidade_inimigo)
-            novo_inimigo.faixa = x  #  adiciona o atributo faixa
+            novo_inimigo.faixa = x  # adiciona o atributo faixa
             inimigos_ativos.append(novo_inimigo)
-    #spawn de mina
+        # spawn de mina
         if agora - tempo_ultimo_spawn_mina > intervalo_spawn_mina:
 
             faixas = [238, 364, 482, 598]  # mesmas faixas dos carros
 
             # Pega todas as faixas já ocupadas por inimigos
             faixas_ocupadas = [inimigo.faixa for inimigo in inimigos_ativos]
-
 
             # Remove faixas ocupadas da lista de spawn disponíveis
             faixas_livres = [faixa for faixa in faixas if faixa not in faixas_ocupadas]
@@ -143,11 +151,6 @@ while run:
                 x = random.choice(faixas_livres)
                 minas_ativas.append(carros.Mina(x, -100, carros.mina, velocidade_mina))
                 tempo_ultimo_spawn_mina = agora
-
-
-
-
-
 
                 # Você pode remover minas que saíram da tela (opcional)
         minas_ativas = [m for m in minas_ativas if m.rect.top < WIN_HEIGHT]
@@ -160,21 +163,32 @@ while run:
             mina.draw(window)
 
         window.blit(carros.carro1, carros.carro1_rect)
-        #pygame.draw.rect(window, (255, 0, 0), hitbox_carro1, 2)
-        #colisao carros
+        # pygame.draw.rect(window, (255, 0, 0), hitbox_carro1, 2)
+        # colisao carros
         for inimigo in inimigos_ativos:
             inimigo.move()
             inimigo.draw(window)
             if hitbox_carro1.colliderect(inimigo.hitbox):
-                game_over = True
 
+                colidiu = True
+                pygame.mixer.music.stop()
+                if colidiu:
+                    if not game_over:
+                        som_batida.play()
+                        pygame.mixer.music.stop()
+                    game_over = True
 
-
-    # Verifica colisão do jogador com minas
+    # colisão com minas
     for mina in minas_ativas:
         if hitbox_carro1.colliderect(mina.hitbox):
 
-            game_over = True
+            colidiu = True
+            pygame.mixer.music.stop()
+            if colidiu:
+                if not game_over:
+                    som_batida.play()
+                    pygame.mixer.music.stop()
+                game_over = True
     if game_over:
         fonte = pygame.font.SysFont("Arial", 60, bold=True)
         texto_game_over = fonte.render("GAME OVER", True, (255, 0, 0))
@@ -188,7 +202,7 @@ while run:
         pygame.display.update()
 
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_RETURN]:  # se apertar ENTER, reinicia
+        if keys[pygame.K_RETURN]:
             # Resetando o jogo
             game_over = False
             score = 0
@@ -199,6 +213,7 @@ while run:
             minas_ativas.clear()
             tempo_ultimo_spawn = pygame.time.get_ticks()
             tempo_ultimo_spawn_mina = pygame.time.get_ticks()
+            pygame.mixer.music.play(-1)
         continue
     fonte_score = pygame.font.SysFont("Arial", 24)
     texto_score_jogo = fonte_score.render(f"Score: {score}", True, (255, 255, 255))
